8ccb1d39a0fe79240dce1c9b3e8e7eaee59e8f40
PhiloSamuel <samuelphilopater@yahoo.com>
2023-05-23 23:41:40 +0300

    client code

--- Assets/Scripts/Client_philo.cs
+++ Assets/Scripts/Client_philo.cs
@@ -1,31 +1,196 @@
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
 
-public class Client_philo : MonoBehaviour
-{
-    private TcpClient client;
-    private NetworkStream stream;
-    private byte[] buffer;
 
-    //my edits ->
-    //public Transform player;
-    //public Vector3 offset;
+public class Client_philo : MonoBehaviour{
+    private Vector3 car2Position;
+    private Vector3 car3Position;
+    private string Name;
+    public Socket sock;
+    private int framecounter;
 
-    // Connect to the server when the game starts
-    //void Start()
-    // {
-    //     client = new TcpClient("127.0.0.1", 8889);
-    //     stream = client.GetStream();
-    //     buffer = new byte[1024];
-    // }
+    private int framestowait=100;
+
+    private bool canplay=false;
+
+    private string message;
+
+
+
+     void recv()
+        {
+            Thread.Sleep(1000);
+
+            while (true)
+            {
+                try
+                { 
+                    Byte[] buffer = new Byte[255];
+                int rec = sock.Receive(buffer, 0, buffer.Length, 0);
+                Array.Resize(ref buffer, rec);
+                string recievedmessage=Encoding.Default.GetString(buffer);
+
+                if (rec <=0) {
+                    Console.WriteLine(Name+"has disconnected");
+                        throw new SocketException();
+                        }
+
+
+                if(recievedmessage=="Now you can play"){
+                    canplay=true;
+                }
+                else if(recievedmessage=="Waiting for 1 Users to connect"){
+                    Debug.Log(recievedmessage);
+
+                    continue;
+                }
+                else{
+                
+                Debug.Log(recievedmessage);
+                string[] parts1 = recievedmessage.Split(',');
+                float x1 = float.Parse(parts1[0]);
+                float y1 = float.Parse(parts1[1]);
+                float z1 = float.Parse(parts1[2]);
+                car2Position = new Vector3(x1, y1, z1);
+
+                }
+
+                }
+                catch {
+                    Debug.Log("The server was closed you are disconnected");
+                    sock.Close();
+                    break;
+                }
+            }
+        }
+          void send()
+        {
+
+            try
+            {
+                while (true)
+                {
+                                Thread.Sleep(800);
+
+                    if(canplay){
+                        
+                    Debug.Log(message);
+
+                    byte[] data = Encoding.ASCII.GetBytes(message); // Convert the string to bytes
+                    sock.Send(data, 0, data.Length, 0);
+                    }
+                   
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+
+
+
+void Start()
+    {
+
+         
+         int port= 5423;
+         IPAddress ip;
+        Name="Philo";
+       /*
+            //   Console.WriteLine("Please Enter Your Name :");
+            //     Name = Console.ReadLine();
+            //     Console.WriteLine("Please Enter the Ip of the Server You want to connect To ");
+            //     ip =  IPAddress.Parse(Console.ReadLine());
+            //     Console.WriteLine("Please Enter the Port of the Server You want to connect To ");
+            //     string inputport = Console.ReadLine();
+
+                // try
+                // {
+                //     port= int.Parse(inputport);
+
+                // }
+                // catch (Exception ex)
+                // {
+                //     port = 5423;
+                // }  
+
+            */    
+        sock = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                //sock.Connect(new IPEndPoint(ip, port));
+                sock.Connect(new IPEndPoint(IPAddress.Parse("192.168.1.10"), 5423));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error connecting");
+            }
+            // Thread rec = new Thread(() => recv(sock));
+            // rec.Start();
+            //Sending the name of the user first before data exchange 
+            byte[] name = Encoding.Default.GetBytes( Name );
+             Console.WriteLine(Name);
+            sock.Send(name, 0, name.Length, 0);
+
+                Byte[] buffer = new Byte[255];
+                int rec1 = sock.Receive(buffer, 0, buffer.Length, 0);
+                Array.Resize(ref buffer, rec1);
+                string recievedmessage=Encoding.Default.GetString(buffer);
+                string[] parts1 = recievedmessage.Split(',');
+                float x1 = float.Parse(parts1[0]);
+                float y1 = float.Parse(parts1[1]);
+                float z1 = float.Parse(parts1[2]);
+
+
+
+
+
+
+
+
+
+        car3Position = new Vector3(x1, y1,z1);
+        GameObject car_3 = GameObject.Find("Car_3"); // Replace "Car1" with the name of your first car object
+         car_3.transform.position = car3Position; 
+
+         Thread  rec = new Thread(() => recv());
+        rec.Start();
+        Thread  sen = new Thread(() => send());
+        sen.Start();
+
+      
+    }
 
     // Send the car coordinates to the server every frame
     void Update()
     {
-        Vector3 carPosition = transform.position; // Get the car position
-        string message = carPosition.x + "," + carPosition.y + "," + carPosition.z; // Convert the position to a string
+
+        framecounter++;
+        if(framecounter>=framestowait){
+
+        car3Position = transform.position; // Get the car position
+        message = car3Position.x + "," + car3Position.y + "," + car3Position.z; // Convert the position to a string
         byte[] data = Encoding.ASCII.GetBytes(message); // Convert the string to bytes
-        stream.Write(data, 0, data.Length); // Send the bytes to the server
+        framecounter=0;
+
+         GameObject car_2 = GameObject.Find("Car_2"); // Replace "Car1" with the name of your first car object
+         car_2.transform.position = car2Position;       
+        }
+    }
+
+
+    public void Close(){
+        sock.Close();
     }
 }
+        
+        
--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -7,6 +7,7 @@ public class EndTrigger : MonoBehaviour {
 	void OnTriggerEnter ()
 	{
 		gameManager.CompleteLevel();
+		FindObjectOfType<Client_philo>().Close();
 	}
 
 }
