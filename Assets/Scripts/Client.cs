3034ee0de473dbafe360904ea4a10248e5276cf8
YoussefShoweter <yoyo10ashraf@gmail.com>
2023-05-25 00:03:50 +0300

    Updated Client

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -8,29 +8,39 @@ using System.Net;
 using System.Text;
 using System.Threading;
 
-
-public class Client_philo : MonoBehaviour{
+public class Client : MonoBehaviour{
     private Vector3 car2Position;
     private Vector3 car3Position;
     private string Name;
     public Socket sock;
     private int framecounter;
 
-    private int framestowait=100;
+    private int framestowait=10;
 
     private bool canplay=false;
 
     private string message;
+    private int matchNum=3;
 
 
+    private void OnApplicationQuit()
+    {
+        try
+        {
+            sock.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
 
-     void recv()
+    void recv()
         {
 
             while (true)
             {
-                            Thread.Sleep(800);
-
+                Thread.Sleep(500);
                 try
                 { 
                     Byte[] buffer = new Byte[255];
@@ -39,12 +49,13 @@ public class Client_philo : MonoBehaviour{
                 string recievedmessage=Encoding.Default.GetString(buffer);
                 string[] parts1 = recievedmessage.Split(',');
                 if (rec <=0) {
-                    Debug.Log(Name+"has disconnected"+"second fail");
+                    Debug.Log(Name+"has disconnected");
                         throw new SocketException();
                         }
 
+                canplay = true;
 
-                if(recievedmessage=="Now you can play"){
+                if (recievedmessage=="Now you can play"){
                     canplay=true;
                 }
                 else if(recievedmessage=="Waiting for 1 Users to connect"){
@@ -70,7 +81,8 @@ public class Client_philo : MonoBehaviour{
                 }
             }
         }
-          void send()
+
+    void send()
         {
 
             try
@@ -95,57 +107,54 @@ public class Client_philo : MonoBehaviour{
 
 
 
-void Start()
-    {
-         int port= 5423;
-         IPAddress ip;
-        Name="Youssef";
-       /*
-            //   Console.WriteLine("Please Enter Your Name :");
-            //     Name = Console.ReadLine();
-            //     Console.WriteLine("Please Enter the Ip of the Server You want to connect To ");
-            //     ip =  IPAddress.Parse(Console.ReadLine());
-            //     Console.WriteLine("Please Enter the Port of the Server You want to connect To ");
-            //     string inputport = Console.ReadLine();
-
-                // try
-                // {
-                //     port= int.Parse(inputport);
-
-                // }
-                // catch (Exception ex)
-                // {
-                //     port = 5423;
-                // }  
-
-            */    
+void Awake()
+{
+        int port= 5423;
+        IPAddress ip;
+        Name="Youssef";  
         sock = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
-            try
-            {
-                //sock.Connect(new IPEndPoint(ip, port));
-                sock.Connect(new IPEndPoint(IPAddress.Parse("192.168.1.38"), 5423));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error connecting");
-            }
-            byte[] name = Encoding.Default.GetBytes( Name );
-             Console.WriteLine(Name);
-            sock.Send(name, 0, name.Length, 0);
+        try
+        {
+            //sock.Connect(new IPEndPoint(ip, port));
+            sock.Connect(new IPEndPoint(IPAddress.Parse("192.168.1.38"), 5423));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error connecting");
+        }
+        byte[] name = Encoding.Default.GetBytes( Name );
+            Console.WriteLine(Name);
+        sock.Send(name, 0, name.Length, 0);
 
-                Byte[] buffer = new Byte[255];
-                int rec1 = sock.Receive(buffer, 0, buffer.Length, 0);
-                Array.Resize(ref buffer, rec1);
-                string recievedmessage=Encoding.Default.GetString(buffer);
-                string[] parts1 = recievedmessage.Split(',');
-                float x1 = float.Parse(parts1[0]);
-                float y1 = float.Parse(parts1[1]);
-                float z1 = float.Parse(parts1[2]);
+
+
+        Byte[] buffer2 = new Byte[255];
+        int rec2 = sock.Receive(buffer2, 0, buffer2.Length, 0);
+        Array.Resize(ref buffer2, rec2);
+        matchNum = BitConverter.ToInt32(buffer2, 0);
+        Debug.Log(matchNum+" <-------This is the server Num");
+        //string recievedMatch = Encoding.Default.GetString(buffer2);
+        //Debug.Log(recievedMatch);
+        //matchNum=int.Parse(Encoding.Default.GetString(buffer2));
+
+
+
+
+
+
+        Byte[] buffer = new Byte[255];
+        int rec1 = sock.Receive(buffer, 0, buffer.Length, 0);
+        Array.Resize(ref buffer, rec1);
+        string recievedmessage=Encoding.Default.GetString(buffer);
+        string[] parts1 = recievedmessage.Split(',');
+        float x1 = float.Parse(parts1[0]);
+        float y1 = float.Parse(parts1[1]);
+        float z1 = float.Parse(parts1[2]);
         car3Position = new Vector3(x1, y1,z1);
         GameObject car_3 = GameObject.Find("Car_3"); // Replace "Car1" with the name of your first car object
-         car_3.transform.position = car3Position; 
+        car_3.transform.position = car3Position; 
 
-         Thread  rec = new Thread(() => recv());
+        Thread  rec = new Thread(() => recv());
         rec.Start();
         Thread  sen = new Thread(() => send());
         sen.Start();
